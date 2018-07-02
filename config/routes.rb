c66b70866bd7b0c16e714732e8a4c02b12d3843d
Vanessa Toro <vanetoro84@gmail.com>
2018-07-02 18:30:43 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   resources :posts, only: [:index, :show, :new, :create, :edit]
+
+  patch 'posts/:id', to: 'posts#update' #adding :update to resourse will make this route available.
+
 end
